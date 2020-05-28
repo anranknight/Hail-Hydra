99378f948d4ff84cc882c6b083566da425202bd9
knight <anranknight@icloud.com>
2020-05-28 23:57:46 +0800

    Solution1

--- 1_两数之和/Solution1.py
+++ 1_两数之和/Solution1.py
@@ -1,9 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for ind, num in enumerate(nums):
-            hashmap[num] = ind
-        for i, num in enumerate(nums):
-            j = hashmap.get(target-num)
-            if j is not None and i!=j:
-                return [i, j]
+def twoSum(nums, target):
+    lens = len(nums)
+    j = -1
+    for i in range(lens):
+        # 判断第二个整数是否在数组中
+        if target - nums[i] in nums:
+            # 判断是否找到的就是第一个整数，如果是则跳出循环
+            if (nums.count(target - nums[i]) == 1) & (target - nums[i] == nums[i]):
+                continue
+            else:
+                # 获取第二个整数的位置
+                j = nums.index(target - nums[i], i+1)
+                break
+    if j > 0:
+        return [i, j]
+    else:
+        return[]
+
---
+++ 1_两数之和/test.py
@@ -0,0 +1,6 @@
+from Solution1 import twoSum
+
+# test input
+nums = [2, 7, 11, 15]
+target = 9
+twoSum(nums, target)
